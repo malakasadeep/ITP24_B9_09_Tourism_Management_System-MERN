3cb751308e23635e884b69ff6c6d44ba93a68b76
malakasadeep <151869451+malakasadeep@users.noreply.github.com>
2023-12-17 00:22:15 +0530

    code indentation

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -7,36 +7,44 @@ export default function SignUp() {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigation = useNavigate();
+
   const handleChange = (e) => {
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
     });
   };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     try {
+      
       setLoading(true);
       const res = await fetch('/api/auth/signup', {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
+        headers: {'Content-Type': 'application/json',},
         body: JSON.stringify(formData),
       });
+
       const data = await res.json();
       console.log(data);
+
       if (data.success === false) {
         setLoading(false);
         setError(data.message);
         return;
       }
+
       setLoading(false);
       setError(null);
       navigation('/sign-in');
+
     } catch (error) {
+
       setLoading(false);
       setError(error.message);
+
     }
   };
 
