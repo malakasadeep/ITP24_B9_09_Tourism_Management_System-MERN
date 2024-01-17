d93585e38c9058f572828cb8b7153f13b2a1674a
malakasadeep <151869451+malakasadeep@users.noreply.github.com>
2024-01-18 01:46:41 +0530

    Complete upload package images functionality

--- client/src/pages/tourPkg/CreatePkg.jsx
+++ client/src/pages/tourPkg/CreatePkg.jsx
@@ -1,6 +1,84 @@
-import React from 'react'
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import React, { useState } from 'react'
+import { app } from '../../firebase';
+import Swal from 'sweetalert2';
+import { MdDeleteForever } from "react-icons/md";
 
 export default function CreatePkg() {
+
+    const [files, setFiles] = useState([]);
+    const [formData, setFormData] = useState({
+        imageUrls: [],
+    });
+    const [fileUploadError, setFileUploadError] = useState(false);
+    const [filePerc, setFilePerc] = useState(0);
+    const [ uploading, setUploading] = useState(false);
+
+    console.log(formData);
+
+    const handleImageSubmit = (e) => {
+        if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+            setUploading(true);
+            setFileUploadError(false);
+            const promises = [];
+
+            for (let i = 0; i < files.length; i++) {
+                promises.push(storeImage(files[i]));
+            }
+            Promise.all(promises).then((urls) => {
+                setFormData({ ...formData, imageUrls: formData.imageUrls.concat(urls) });
+                setFileUploadError(false);
+                setUploading(false);
+            }).catch((err) => {
+                setFileUploadError(Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "Image upload failed (2MB max)",
+                  }));
+                  setUploading(false);
+            });
+        }else{
+            setFileUploadError(Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "You can upload max 6 images",
+              }));
+              setUploading(false);
+        }
+    };
+
+    const storeImage = async (file) => {
+        return new Promise( (resolve, reject) => {
+            const storage = getStorage(app);
+            const fileName = new Date().getTime() + file.name;
+            const storageRef = ref(storage , `packageImages/${fileName}`);
+            const uploadTask = uploadBytesResumable(storageRef, file)
+            uploadTask.on(
+                'state_changed',
+                (snapshot) => {
+                    const progress = (snapshot.bytesTransferred/snapshot.totalBytes) * 100;
+                    setFilePerc(Math.round(progress));
+                    console.log('Upload is ' + progress + '% done');
+                },
+                (error)=>{
+                    reject(error);
+                },
+                ()=>{
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        resolve(downloadURL);
+                    });
+                }
+                );
+        });
+    };
+
+    const handleremoveImage = (index) => {
+        setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.filter((_, i) => i !== index),
+        })
+    };
+
   return (
     <main className='p-3 max-w-4xl mx-auto'>
         <h1 className='text-3xl font-semibold text-center my-7 mt-24'>Create a New Package</h1>
@@ -94,9 +172,27 @@ export default function CreatePkg() {
                 <span className='font-normal text-gray-600 ml-2'>The first image will be the cover (max 6)</span>
                 </p>
                 <div className='flex gap-4'>
-                    <input type='file' className='p-3 border border-blue-700 rounded w-full' id='images' accept='image/*' multiple required />
-                    <button className='p-3 text-blue-700 border border-blue-700 rounded uppercase hover:shadow-xl disabled:opacity-80'>Upload</button>
+                    <input onChange={(e) => setFiles(e.target.files)} type='file' className='p-3 border border-blue-700 rounded w-full' id='images' accept='image/*' multiple required />
+                    <button  onClick={handleImageSubmit} className='p-3 text-blue-700 border border-blue-700 rounded uppercase hover:shadow-xl disabled:opacity-80' disabled = {uploading} >{uploading ? 'Uploading...' : 'Upload'}</button>
                 </div>
+                <p className='text-sm self-center font-semibold'>
+                    {fileUploadError ?
+                        (<span className='text-red-700'>Error image upload</span>) :
+                    filePerc > 0 && filePerc < 100 ? 
+                        (<span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>) :
+                    filePerc === 100 ? 
+                        (<span className='text-green-700'>Image upload successfully!!</span>) :
+                        ("")
+                     }
+                </p>
+                {
+                    formData.imageUrls.length > 0 && formData.imageUrls.map((url, index) => (
+                        <div key={url} className='flex justify-between p-3 border border-blue-700 items-center'>
+                            <img src={url} alt="pkg images" className='w-24 h-24 object-contain rounded-lg' />
+                            <button type='button' onClick={() => handleremoveImage(index)} className='text-red-700 text-5xl font-extrabold rounded-lg uppercase hover:opacity-60'><MdDeleteForever /></button>
+                        </div>
+                    ))
+                }
                 <button className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>List the Package</button>
             </div>
         </form>
