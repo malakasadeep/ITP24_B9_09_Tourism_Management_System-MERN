b5cd8f0c368fd784410a4c631fe64a936fcc75e3
Dewni Jayasuriya <dewnijayasuriya123@gmail.com>
2024-03-07 00:31:13 +0530

    create a form

--- client/src/components/headers/Layout.jsx
+++ client/src/components/headers/Layout.jsx
@@ -1,45 +1,54 @@
-import React, { createContext, useContext, useEffect} from 'react'
+import React, { createContext, useContext, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import Header from './Header';
-import SellerHeader from './SellerHeader';
-import Adminheader from './Adminheader';
-
+import Header from "./Header";
+import SellerHeader from "./SellerHeader";
+import Adminheader from "./Adminheader";
 
 const Layout = () => {
+  const INITIAL_STATE = {
+    user: JSON.parse(localStorage.getItem("user")) || null,
+    loading: false,
+    error: null,
+  };
 
-    const INITIAL_STATE = {
-        user: JSON.parse(localStorage.getItem("user")) || null,
-        loading: false,
-        error: null,
-      };
+  const AuthContext = createContext(INITIAL_STATE);
 
-    const AuthContext = createContext(INITIAL_STATE);  
+  const { user } = useContext(AuthContext);
 
-    const { user } = useContext(AuthContext);
+  const location = useLocation();
 
-    const location = useLocation();
+  const showHeader =
+    location.pathname === "/" ||
+    location.pathname === "/sign-up" ||
+    location.pathname === "/sign-in" ||
+    location.pathname === "/profile" ||
+    location.pathname.startsWith("/packages/") ||
+    location.pathname === "/package-search";
 
-    const showHeader = location.pathname === "/" || 
-                       location.pathname === '/sign-up' || 
-                       location.pathname === "/sign-in" || 
-                       location.pathname === "/profile"||
-                       location.pathname.startsWith('/packages/') ||
-                       location.pathname === "/package-search";
+  const showSellerHeader =
+    location.pathname === "/additems" ||
+    location.pathname === "/seller/profile" ||
+    location.pathname === "/create-package" ||
+    location.pathname === "/my-items" ||
+    location.pathname.startsWith("/update-package/") ||
+    location.pathname.startsWith("/my-packages/") ||
+    location.pathname.startsWith("/events/");
 
-    const showSellerHeader = location.pathname === "/additems" || 
-                             location.pathname === "/seller/profile" ||
-                             location.pathname === "/create-package" || 
-                             location.pathname === "/my-items" || 
-                             location.pathname.startsWith('/update-package/') ||
-                             location.pathname.startsWith('/my-packages/');
-    
-    const showAdminHeader = location.pathname === "/admin" || 
-                            location.pathname === "/admin/profile";
+  const showAdminHeader =
+    location.pathname === "/admin" || location.pathname === "/admin/profile";
   return (
     <div>
-        {showAdminHeader? (<Adminheader/>) : showSellerHeader? (<SellerHeader/>): showHeader? (<Header/>) : ''}
+      {showAdminHeader ? (
+        <Adminheader />
+      ) : showSellerHeader ? (
+        <SellerHeader />
+      ) : showHeader ? (
+        <Header />
+      ) : (
+        ""
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Layout;
