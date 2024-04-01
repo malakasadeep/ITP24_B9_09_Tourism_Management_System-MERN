986f32a8d93c905ad079854eac75e25aa7675d23
Tharindu wikasith dilshan <127284955+tharinduWik@users.noreply.github.com>
2024-04-01 17:40:54 +0530

    fix error

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,19 +1,14 @@
-import Express from "express";
-import {
-  createhotel,
-  deletehotel,
-  gethotel,
-  gethotelsSearch,
-  updatehotel,
-} from "../controllers/hotel.js";
-import { veryfyTocken } from "../utils/verifyUser.js";
+// import  Express  from "express";
+// import { createhotel, deletehotel, gethotel, gethotelsSearch, updatehotel } from "../controllers/package.controller.js";
+// import { veryfyTocken } from "../utils/verifyUser.js";
 
-const router = Express.Router();
+// const router = Express.Router();
 
-router.get("/gethotels", gethotelsSearch);
-router.post("/createhotel", createhotel);
-router.delete("/delete/:id", veryfyTocken, deletehotel);
-router.post("/update/:id", veryfyTocken, updatehotel);
-router.get("/get-update/:id", gethotel);
+// router.post('/createhotel', veryfyTocken, createhotel);
+// router.delete('/delete/:id', veryfyTocken, deletehotel);
+// router.post('/update/:id', veryfyTocken, updatehotel);
+// router.get('/get-update/:id', gethotel);
+// router.get('/gethotels', gethotelsSearch);
 
-export default router;
+
+// export default router;
