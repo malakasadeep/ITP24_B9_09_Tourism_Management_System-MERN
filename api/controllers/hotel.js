b443e5b260ef5a95a9fb20447175ee234dffb00d
malakasadeep <151869451+malakasadeep@users.noreply.github.com>
2024-04-10 20:31:03 +0530

    Refactor hotel routes and controllers, update imports in HotelCard.jsx

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -1,164 +1,148 @@
-
-import express from 'express';
-import Hotel from "../models/Hotel.js"
-import multer from "multer"
-import path from "path"
-import Hotel from "../models/Hotel.js"
+import express from "express";
+import Hotel from "../models/Hotel.js";
+import multer from "multer";
+import path from "path";
 import { errorHandler } from "../utils/error.js";
 
-
-  
- // create Hotel//
- export const createhotel = async (req, res, next) => {
-
+// create Hotel//
+export const createhotel = async (req, res, next) => {
   try {
-
-      const hotelListning = await Hotel.create(req.body);
-      return res.status(201).json(hotelListning);
-      
-  } catch (error){
-      next(error);
-      
+    const hotelListning = await Hotel.create(req.body);
+    return res.status(201).json(hotelListning);
+  } catch (error) {
+    next(error);
   }
-}
+};
 //delete Hotel//
 export const deletehotel = async (req, res, next) => {
   const hotel = await hotelListning.findById(req.params.id);
 
   if (!hotel) {
-      return next(errorHandler(404, 'Hotel not found'));
+    return next(errorHandler(404, "Hotel not found"));
   }
 
-  if(req.user.id !== pkg.userRef){
-      return next(errorHandler(401, 'you can delete your own Hotel'))
+  if (req.user.id !== pkg.userRef) {
+    return next(errorHandler(401, "you can delete your own Hotel"));
   }
 
   try {
-      await hotelListning.findByIdAndDelete(req.params.id);
-      res.status(200).json('Hotel deleted')
+    await hotelListning.findByIdAndDelete(req.params.id);
+    res.status(200).json("Hotel deleted");
   } catch (error) {
-      next(error);
+    next(error);
   }
-}
+};
 //update Hotel//
 export const updatehotel = async (req, res, next) => {
   const hotel = await hotelListning.findById(req.params.id);
 
   if (!hotel) {
-      return next(errorHandler(404, 'Hotel not found'));
+    return next(errorHandler(404, "Hotel not found"));
   }
 
-  if(req.user.id !== hotel.userRef){
-      return next(errorHandler(401, 'you can update your own Hotel'))
+  if (req.user.id !== hotel.userRef) {
+    return next(errorHandler(401, "you can update your own Hotel"));
   }
 
   try {
-      const updatedhotel = await hotelListning.findByIdAndUpdate(req.params.id, req.body, {new: true});
-      res.status(200).json(updatedhotel)
+    const updatedhotel = await hotelListning.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedhotel);
   } catch (error) {
-      next(error);
+    next(error);
   }
 };
- 
+
 //get Hotel//
 export const gethotel = async (req, res, next) => {
-    
   try {
-      const hotel = await hotelListning.findById(req.params.id);
-      if (!hotel) {
-          return next(errorHandler(404, 'Hotel not found'));
-      }
-      res.status(200).json(hotel)
+    const hotel = await hotelListning.findById(req.params.id);
+    if (!hotel) {
+      return next(errorHandler(404, "Hotel not found"));
+    }
+    res.status(200).json(hotel);
   } catch (error) {
-      next(error);
+    next(error);
   }
 };
 
 //get all Hotels
 export const getAllHotel = async (req, res, next) => {
-    const { min, max, ...others } = req.query;
-    try {
-      const hotels = await Hotel.find({
-        ...others,
-        isApproved: true,
-        cheapestPrice: { $gt: min | 1, $lt: max || 100000 }
-      }).limit(req.query.limit);
-      res.status(200).json(hotels);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  };
+  const { min, max, ...others } = req.query;
+  try {
+    const hotels = await Hotel.find({
+      ...others,
+      isApproved: true,
+      cheapestPrice: { $gt: min | 1, $lt: max || 100000 },
+    }).limit(req.query.limit);
+    res.status(200).json(hotels);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
 
 //search hotel
 
 export const gethotelsSearch = async (req, res, next) => {
   try {
-      const limit = parseInt(req.query.limit) || 9;
-      const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
 
-      let featured = req.query.featured;
-      if (featured === undefined || featured === 'false'){
-          featured = {$in: [false, true]};
-      }
+    let featured = req.query.featured;
+    if (featured === undefined || featured === "false") {
+      featured = { $in: [false, true] };
+    }
 
-      let availableWork = req.query.availableWork;
-      if (availableWork === undefined || availableWork === 'false'){
-        availableWork = {$in: [false, true]};
-      }
+    let availableWork = req.query.availableWork;
+    if (availableWork === undefined || availableWork === "false") {
+      availableWork = { $in: [false, true] };
+    }
 
-      let sustainability = req.query.sustainability;
-      if (sustainability === undefined || sustainability === 'false'){
-        sustainability = {$in: [false, true]};
-      }
+    let sustainability = req.query.sustainability;
+    if (sustainability === undefined || sustainability === "false") {
+      sustainability = { $in: [false, true] };
+    }
 
-      let type = req.query.type;
-      if (type === undefined || type === 'all'){
-          type = {$in: ['reguler', 'couple', 'family']};
-      }
+    let type = req.query.type;
+    if (type === undefined || type === "all") {
+      type = { $in: ["reguler", "couple", "family"] };
+    }
 
-      let hoteltype = req.query.hoteltype;
-      if (hoteltype === undefined || hoteltype === 'all'){
-          hoteltype = {$in: ['3 Star Hotel', '4 Star Hotel', '5 Star Hotel']};
-      }
+    let hoteltype = req.query.hoteltype;
+    if (hoteltype === undefined || hoteltype === "all") {
+      hoteltype = { $in: ["3 Star Hotel", "4 Star Hotel", "5 Star Hotel"] };
+    }
 
-      const searchTerm = req.query.searchTerm || '';
+    const searchTerm = req.query.searchTerm || "";
 
-      const province = req.query.province || '';
-      
-      const city = req.query.city || '';
+    const province = req.query.province || "";
 
-      const sort = req.query.sort || 'createdAt';
+    const city = req.query.city || "";
 
-      const order = req.query.order || 'desc';
+    const sort = req.query.sort || "createdAt";
 
+    const order = req.query.order || "desc";
 
-      const pkgs = await HotelListning.find({
-        name: {$regex: searchTerm, $options: 'i'},
-        province: {$regex: province, $options: 'i'},
-        city: {$regex: city, $options: 'i'},
+    const pkgs = await Hotel.find({
+      name: { $regex: searchTerm, $options: "i" },
+      // province: { $regex: province, $options: "i" },
+      //city: { $regex: city, $options: "i" },
 
-          days: {$gte: days},
-          featured,
-          availableWork,
-          sustainability,
-          type,
-          hoteltype,
-      })
-      .sort({[sort]: order})
+      // featured,
+      //availableWork,
+      //sustainability,
+      //type,
+      //hoteltype,
+    })
+      .sort({ [sort]: order })
       .limit(limit)
       .skip(startIndex);
 
-      return res.status(200).json(pkgs);
-
-
+    return res.status(200).json(pkgs);
   } catch (error) {
-      next(error);
+    next(error);
   }
 };
-
-
-
-
-
-
-
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,125 +1,158 @@
-import React, { useEffect, useState } from 'react'
-import PackageCard from '../components/tour-packages/PackageCard';
-import HotelCard from '../components/hotel/HotelCard';
-import { Link } from 'react-router-dom';
-import'./../assets/css/Home.css';
-import Bubbles from '../components/Bubbles';
+import React, { useEffect, useState } from "react";
+import PackageCard from "../components/tour-packages/PackageCard";
+import HotelCard from "../components/hotel/HotelCard";
+import { Link } from "react-router-dom";
+import "./../assets/css/Home.css";
+import Bubbles from "../components/Bubbles";
 
 export default function Home() {
-
   const [packages, setPackages] = useState([]);
-  const [hotels, sethotels] = useState([])
-  
+  const [hotels, sethotels] = useState([]);
+
   useEffect(() => {
     const fetchPkg = async () => {
       try {
-        const res = await fetch('/api/Package/getpkgs?limit=3');
+        const res = await fetch("/api/Package/getpkgs?limit=3");
         const data = await res.json();
         setPackages(data);
       } catch (error) {
         console.log(error);
-      } 
-    }
-    fetchPkg();   
-  }, [])
+      }
+    };
+    fetchPkg();
+  }, []);
+
+  useEffect(() => {
+    const fetchHotels = async () => {
+      try {
+        const res = await fetch("/api/hotel/gethotels?limit=3");
+        const data = await res.json();
+        sethotels(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchHotels();
+  }, []);
 
   return (
-    
-   // <div style={{ height: '100%',  backgroundSize: "cover",backgroundPosition: "center", backgroundRepeat: "no-repeat*/"}}>
+    // <div style={{ height: '100%',  backgroundSize: "cover",backgroundPosition: "center", backgroundRepeat: "no-repeat*/"}}>
     //  <h1 className='text-3xl'>Home</h1>
     <div>
-      
-      
-<div id="parallax-world-of-ugg">
-  
-  <section>
+      <div id="parallax-world-of-ugg">
+        <section>
+          <div class="title"></div>
+          <Bubbles />
+        </section>
 
-    <div class="title">
-    </div>
-    <Bubbles/>
-  </section>
-  
-  <section>
-      <div className="parallax-one">
-        <h2>EXPLORE SRI LANKA</h2>
+        <section>
+          <div className="parallax-one">
+            <h2>EXPLORE SRI LANKA</h2>
+          </div>
+        </section>
+
+        <section>
+          <div className="block">
+            <p>
+              <span className="first-character sc">T</span>he "TourCraft” is the
+              one of the leading pioneers of providing innovative technology
+              solutions for the tourism industry. With over a decade of
+              experience, we have been at the forefront of designing modern
+              software applications that assist tour operators, tour agencies.
+              Our proposed web-application " TourCraft " allows travelers to
+              connect with each other, make their travel journey in a
+              pleasurable manner. They can easily collaborate with our staff
+              members been declining since the company advertisements have no or
+              less exposure to the online digital market tendency. Moreover,
+              current social media applications are not specialized for
+              travelling purposes. To address this adverse impact, this
+              user-centric software application supports travel specific
+              functions such as: travel planning, finding users interested in
+              same travel destination spots, sharing real travel experiences etc
+            </p>
+          </div>
+        </section>
+
+        <section>
+          <div className="parallax-three">
+            <h2>ENCHANTED FOREST</h2>
+          </div>
+        </section>
+
+        <section>
+          <div className="block">
+            <p>
+              <span className="first-character atw">W</span>hen the New York
+              fashion community notices your brand, the world soon follows. The
+              widespread love for UGG extended to Europe in the mid-2000's along
+              with the stylish casual movement and demand for premium casual
+              fashion. UGG boots and shoes were now seen walking the streets of
+              London, Paris and Amsterdam with regularity. To meet the rising
+              demand from new fans, UGG opened flagship stores in the UK and an
+              additional location in Moscow. As the love spread farther East,
+              concept stores were opened in Beijing, Shanghai and Tokyo. UGG
+              Australia is now an international brand that is loved by all. This
+              love is a result of a magical combination of the amazing
+              functional benefits of sheepskin and the heightened emotional
+              feeling you get when you slip them on your feet. In short, you
+              just feel better all over when you wear UGG boots, slippers, and
+              shoes.
+            </p>
+            <p className="line-break margin-top-10"></p>
+          </div>
+        </section>
       </div>
-  </section>
-  
-  <section>
-    <div className="block">
-      <p><span className="first-character sc">T</span>he "TourCraft” is the one of the leading pioneers of providing innovative technology solutions for 
-the tourism industry. With over a decade of experience, we have been at the forefront of 
-designing modern software applications that assist tour operators, tour agencies.
-Our proposed web-application " TourCraft " allows travelers to connect with each other, make 
-their travel journey in a pleasurable manner.
-They can easily collaborate with our staff members been declining since the company 
-advertisements have no or less exposure to the online digital market tendency.
-Moreover, current social media applications are not specialized for travelling purposes. To 
-address this adverse impact, this user-centric software application supports travel specific 
-functions such as: travel planning, finding users interested in same travel destination spots, 
-sharing real travel experiences etc</p>
-     
-    </div>
-  </section>
-  
-
-  
-  <section>
-  <div className="parallax-three">
-    <h2>ENCHANTED FOREST</h2>
-  </div>
-</section>
-
-<section>
-  <div className="block">
-    <p><span className="first-character atw">W</span>hen the New York fashion community notices your brand, the world soon follows. The widespread love for UGG extended to Europe in the mid-2000's along with the stylish casual movement and demand for premium casual fashion. UGG boots and shoes were now seen walking the streets of London, Paris and Amsterdam with regularity. To meet the rising demand from new fans, UGG opened flagship stores in the UK and an additional location in Moscow. As the love spread farther East, concept stores were opened in Beijing, Shanghai and Tokyo. UGG Australia is now an international brand that is loved by all. This love is a result of a magical combination of the amazing functional benefits of sheepskin and the heightened emotional feeling you get when you slip them on your feet. In short, you just feel better all over when you wear UGG boots, slippers, and shoes.</p>
-    <p className="line-break margin-top-10"></p>
-  
-  </div>
-</section>
-  
-  
-  </div>
       <div>
-      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10'>
-        {packages && packages.length > 0 && (
-          <div className=''>
-            <div className='my-3'>
-              <h2 className='text-2xl font-semibold text-slate-600'>Recent Packages</h2>
-              <Link className='text-sm text-blue-800 hover:underline' to={'/package-search'}>Show more Packages</Link>
+        <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
+          {packages && packages.length > 0 && (
+            <div className="">
+              <div className="my-3">
+                <h2 className="text-2xl font-semibold text-slate-600">
+                  Recent Packages
+                </h2>
+                <Link
+                  className="text-sm text-blue-800 hover:underline"
+                  to={"/package-search"}
+                >
+                  Show more Packages
+                </Link>
+              </div>
+
+              <div className="flex flex-wrap gap-10">
+                {packages.map((pkg) => (
+                  <PackageCard pkg={pkg} key={pkg._id} />
+                ))}
+              </div>
             </div>
-        
-            <div className='flex flex-wrap gap-10'>
-              {packages.map((pkg) => (
-                <PackageCard pkg={pkg} key={pkg._id} />
-              ))}
-            </div>
-            </div>
-        
-        )}
+          )}
         </div>
       </div>
 
+      {/*<div>
+        <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
+          {hotels && hotels.length > 0 && (
+            <div className="">
+              <div className="my-3">
+                <h2 className="text-2xl font-semibold text-slate-600">
+                  Recent Hotels
+                </h2>
+                <Link
+                  className="text-sm text-blue-800 hover:underline"
+                  to={"/hotel-search"}
+                >
+                  Show more Hotel
+                </Link>
+              </div>
 
-      <div>
-      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10'>
-        {hotels && hotels.length > 0 && (
-          <div className=''>
-            <div className='my-3'>
-              <h2 className='text-2xl font-semibold text-slate-600'>Recent Hotels</h2>
-              <Link className='text-sm text-blue-800 hover:underline' to={'/hotel-search'}>Show more Hotel</Link>
+              <div className="flex flex-wrap gap-10">
+                {hotels.map((hotell) => (
+                  <HotelCard hotell={hotell} key={hotell._id} />
+                ))}
+              </div>
             </div>
-        
-            <div className='flex flex-wrap gap-10'>
-              {hotels.map((hotell) => (
-                <HotelCard hotell={hotell} key={hotell._id} />
-              ))}
-            </div>
-            </div>
-        
-        )}
+          )}
         </div>
-      </div>
+        </div>*/}
     </div>
-  )
+  );
 }
