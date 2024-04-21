091f1df3675b3eba8af29f15ed8a9d0de0d65916
malakasadeep <151869451+malakasadeep@users.noreply.github.com>
2024-04-22 03:58:11 +0530

    Refactor AdminUM component and Home page

--- client/src/components/user/AdminUM.jsx
+++ client/src/components/user/AdminUM.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
-import { AdminCard } from '../AdminCard'
-import { UserList } from './UserList'
+import React from "react";
+import { AdminCard } from "../AdminCard";
+import { UserList } from "./UserList";
 
-import '../../assets/css/adminContent.css'
-import { Sidebar } from '../Sidebar'
-import { Route, Routes } from 'react-router-dom'
+import "../../assets/css/adminContent.css";
+import { Sidebar } from "../Sidebar";
+import { Route, Routes } from "react-router-dom";
 
 export const AdminUM = () => {
   return (
-    <div className='dashboard' style={{background:'#dde6ed', padding:'20px'}}>
-        <Sidebar/>
-        <div className="dashboard--content">
-            <div>
-            <Routes>
-                <Route path='/' element={<UserList/>} />
-            </Routes>
-            </div>
-        </div>    
+    <div
+      className="dashboard"
+      style={{ background: "#dde6ed", padding: "20px" }}
+    >
+      <Sidebar />
+      <div className="dashboard--content">
+        <div>
+          <Routes>
+            <Route path="/" element={<UserList />} />
+          </Routes>
+        </div>
+      </div>
     </div>
-    
-  )
-}
+  );
+};
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,7 +22,7 @@ export default function Home() {
     fetchPkg();
   }, []);
 
-  /*useEffect(() => {
+  useEffect(() => {
     const fetchHotels = async () => {
       try {
         const res = await fetch("/api/hotel/gethotels?limit=3");
@@ -34,7 +34,7 @@ export default function Home() {
       }
     };
     fetchHotels();
-  }, []);*/
+  }, []);
 
   return (
     // <div style={{ height: '100%',  backgroundSize: "cover",backgroundPosition: "center", backgroundRepeat: "no-repeat*/"}}>
@@ -129,31 +129,33 @@ export default function Home() {
         </div>
       </div>
 
-      {<div>
-        <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
-          {hotels && hotels.length > 0 && (
-            <div className="">
-              <div className="my-3">
-                <h2 className="text-2xl font-semibold text-slate-600">
-                  Recent Hotels
-                </h2>
-                <Link
-                  className="text-sm text-blue-800 hover:underline"
-                  to={"/hotel-search"}
-                >
-                  Show more Hotel
-                </Link>
-              </div>
+      {
+        <div>
+          <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
+            {hotels && hotels.length > 0 && (
+              <div className="">
+                <div className="my-3">
+                  <h2 className="text-2xl font-semibold text-slate-600">
+                    Recent Hotels
+                  </h2>
+                  <Link
+                    className="text-sm text-blue-800 hover:underline"
+                    to={"/hotel-search"}
+                  >
+                    Show more Hotel
+                  </Link>
+                </div>
 
-              <div className="flex flex-wrap gap-10">
-                {hotels.map((hotell) => (
-                  <HotelCard hotell={hotell} key={hotell._id} />
-                ))}
+                <div className="flex flex-wrap gap-10">
+                  {hotels.map((hotell) => (
+                    <HotelCard hotell={hotell} key={hotell._id} />
+                  ))}
+                </div>
               </div>
-            </div>
-          )}
+            )}
+          </div>
         </div>
-        </div>}
+      }
     </div>
   );
 }
