516ca920b0d9dc499612ded6770c9423c5a0b294
prabodhi900 <151753850+prabodhi900@users.noreply.github.com>
2024-04-22 16:02:51 +0530

    add tour guid functions

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import PackageCard from "../components/tour-packages/PackageCard";
 import HotelCard from "../components/hotel/HotelCard";
 import { Link } from "react-router-dom";
 import "./../assets/css/Home.css";
+import guidimg from "./../assets/img/4670.gif"
 import Bubbles from "../components/Bubbles";
 
 export default function Home() {
@@ -146,7 +147,7 @@ export default function Home() {
                   </Link>
                 </div>
 
-                <div className="flex flex-wrap gap-10">
+                <div className="flex flex-wrap gap-16">
                   {hotels.map((hotell) => (
                     <HotelCard hotell={hotell} key={hotell._id} />
                   ))}
@@ -156,6 +157,25 @@ export default function Home() {
           </div>
         </div>
       }
+      <div>
+          <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
+            {hotels && hotels.length > 0 && (
+              <div className="">
+                <div className="my-3">
+                  
+                  <Link
+                    className="text-sm text-blue-800 hover:underline"
+                    to={"/guid/search"}
+                  >
+                    <img src={guidimg}/>
+                  </Link>
+                </div>
+
+                
+              </div>
+            )}
+          </div>
+        </div>
     </div>
   );
 }
