5faf810590349806f078d2e677e68ead739dd49f
Dewni Jayasuriya <dewnijayasuriya123@gmail.com>
2024-05-15 16:28:51 +0530

    create event calendar

--- client/src/components/eventAct/EventCalendar.jsx
+++ client/src/components/eventAct/EventCalendar.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from "react";
 import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid";
+import dayGridPlugin from "@fullcalendar/daygrid";//plugin for FullCalendar that provides a basic "grid" view.
 
 function EventCalendarPage() {
-  const [events, setEvents] = useState([]);
+  const [events, setEvents] = useState([]);// store the events fetched from the API.
 
   useEffect(async () => {
-    const res = await fetch(`/api/events/`);
+    const res = await fetch(`/api/events/`);// fetches events data from the API
     const data = await res.json();
     const formattedEvents = data.map((event) => {
-      const backgroundColor = event.type === "Activity" ? "#FF0000" : "#00FF00";
+      const backgroundColor = event.type === "Activity" ? "#FF0000" : "#00FF00";//if event type is activity then backgroundColor is green. if not red.
       const textColor = event.type === "Activity" ? "#FFFFFF" : "#000000";
       const borderColor = backgroundColor;
       return {
